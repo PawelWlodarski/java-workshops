680d84f769f619ee4ebe688569525d6c03e8846b
pawel <pwlodars@gmail.com>
2025-04-06 18:13:09 +0200

    JEP495 Simple Java Source

---
+++ src/main/java/com/wlodar/jeeps/jep495simplesourcefiles/JustSimpleSourceDemo.java
@@ -0,0 +1,9 @@
+package com.wlodar.jeeps.jep495simplesourcefiles;
+
+import java.io.IO;
+
+public class JustSimpleSourceDemo {
+    public void main() {
+        IO.println("This is an instance method without args not static and you can run it in IDEA");
+    }
+}
---
+++ src/main/java/com/wlodar/jeeps/jep495simplesourcefiles/TheSimplestExampleWithJustMainFunction.java
@@ -0,0 +1,6 @@
+public void main() {
+    println("As simple as that");
+}
+
+//you can run it in main
+String greeting() { return "Hello, World!"; }
